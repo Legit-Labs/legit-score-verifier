6599ed37caa9a6d48be3f531bc28e0ea89f9567f
gal-legit <gal@legitsecurity.com>
2022-09-05 13:39:24 +0300

    implemented a simple wrapper

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+
+	legitscore "github.com/legit-labs/legit-score"
+	verifyattestation "github.com/legit-labs/legit-verify-attestation/verify-attestation"
+)
+
+var (
+	keyPath         string
+	attestationPath string
+	repo            string
+	minScore        float64
+)
+
+func main() {
+	flag.StringVar(&keyPath, "key", "", "The path of the public key")
+	flag.StringVar(&attestationPath, "attestation", "", "The path of the attestation document")
+	flag.Float64Var(&minScore, "min-score", 0, "The minimal Legit score")
+	flag.StringVar(&repo, "repository", "", "The repository in question")
+
+	flag.Parse()
+
+	if keyPath == "" {
+		log.Panicf("please provide a public key path")
+	} else if attestationPath == "" {
+		log.Panicf("please provide an attestation path")
+	} else if repo == "" {
+		log.Panicf("please provide a repository")
+	}
+
+	attestation, err := os.ReadFile(attestationPath)
+	if err != nil {
+		log.Panicf("failed to open attestation at %v: %v", attestationPath, err)
+	}
+
+	payload, err := verifyattestation.VerifiedPayload(context.Background(), keyPath, attestation)
+	if err != nil {
+		log.Panicf("attestation verification failed: %v", err)
+	}
+
+	var predicate legitscore.LegitScorePredicate
+	err = json.Unmarshal(payload, &predicate)
+	if err != nil {
+		log.Panicf("failed to unmarshal predicate: %v", err)
+	}
+
+	if err = predicate.Verify(repo, minScore); err != nil {
+		log.Panicf("score verification failed: %v", err)
+	}
+
+	log.Printf("repository and score were verified successfully.")
+}
